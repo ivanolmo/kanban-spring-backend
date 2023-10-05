52beb862b98f737b1fddc18c6c1545dec2cb45a3
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-05 13:52:03 -0500

    Add Subtask entity

---
+++ src/main/java/com/ivanolmo/kanbantaskmanager/entity/Subtask.java
@@ -0,0 +1,37 @@
+package com.ivanolmo.kanbantaskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "subtasks")
+@EntityListeners(AuditingEntityListener.class)
+public class Subtask {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "completed", nullable = false)
+  private Boolean completed = false;
+
+  @ManyToOne
+  @JoinColumn(name = "task_id")
+  private Task task;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+}
