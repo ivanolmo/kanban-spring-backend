31fedeb198a5addb028fb829c91c4ed92984c7e7
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-05 13:44:15 -0500

    Add Task entity

---
+++ src/main/java/com/ivanolmo/kanbantaskmanager/entity/Task.java
@@ -0,0 +1,45 @@
+package com.ivanolmo.kanbantaskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "title", nullable = false)
+  private String title;
+
+  @Column(name = "description", nullable = false)
+  private String description;
+
+  @Column(name = "completed", nullable = false)
+  private Boolean completed = false;
+
+  @ManyToOne
+  @JoinColumn(name = "board_column_id")
+  private BoardColumn boardColumn;
+
+  @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Subtask> subtasks = new ArrayList<>();
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+}
