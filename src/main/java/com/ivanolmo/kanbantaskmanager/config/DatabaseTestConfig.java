073f1aa63fe85c670861f0f236179cf7495da26c
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-05 18:48:12 -0500

    Add config files to own package

--- src/main/java/com/ivanolmo/kanbantaskmanager/config/DatabaseTestConfig.java
+++ src/main/java/com/ivanolmo/kanbantaskmanager/config/DatabaseTestConfig.java
@@ -1,4 +1,4 @@
-package com.ivanolmo.kanbantaskmanager;
+package com.ivanolmo.kanbantaskmanager.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
