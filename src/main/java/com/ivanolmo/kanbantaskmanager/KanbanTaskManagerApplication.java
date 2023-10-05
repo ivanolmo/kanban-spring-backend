d49c086f45a9a695a8b8f9e6550889aaa0a309f0
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-04 22:13:01 -0500

    Initial commit for Spring Boot backend

---
+++ src/main/java/com/ivanolmo/kanbantaskmanager/KanbanTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ivanolmo.kanbantaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanTaskManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KanbanTaskManagerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivanolmo/kanbantaskmanager/KanbanTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanolmo.kanbantaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanTaskManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
