f5e9180d15fa916befc106cb9d5174283e04c6a5
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-09 16:41:49 -0500

    Refactor board controller exception handling
    Moved all exception handling to a global exception handler.

--- src/main/java/com/ivanolmo/kanbantaskmanager/controller/BoardController.java
+++ src/main/java/com/ivanolmo/kanbantaskmanager/controller/BoardController.java
@@ -1,12 +1,16 @@
 package com.ivanolmo.kanbantaskmanager.controller;
 
 import com.ivanolmo.kanbantaskmanager.entity.Board;
+import com.ivanolmo.kanbantaskmanager.exception.BoardNotFoundException;
+import com.ivanolmo.kanbantaskmanager.exception.UserNotFoundException;
 import com.ivanolmo.kanbantaskmanager.service.BoardService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,85 +25,65 @@ public class BoardController {
     this.boardService = boardService;
   }
 
-
   @GetMapping
-  public ResponseEntity<?> getAllUserBoards(@RequestParam Long userId) {
-    try {
-      if (userId <= 0) {
-        return new ResponseEntity<>("Invalid user id.", HttpStatus.BAD_REQUEST);
-      }
-
-      List<Board> boards = boardService.getAllUserBoards(userId);
-      if (boards.isEmpty()) {
-        return new ResponseEntity<>("No boards found for this user.", HttpStatus.NOT_FOUND);
-      }
-
-      return new ResponseEntity<>(boards, HttpStatus.OK);
-    } catch (Exception e) {
-      logger.error("An error occurred: {}", e.getMessage());
-      return new ResponseEntity<>("An internal error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+  public ResponseEntity<?> getAllUserBoards(@RequestParam Long userId, HttpServletRequest request) {
+    CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    if (csrfToken != null) {
+      System.out.println("CSRF token from request attribute: " + csrfToken.getToken());
     }
+
+    if (userId <= 0) {
+      throw new UserNotFoundException("Invalid user id.");
+    }
+
+    List<Board> boards = boardService.getAllUserBoards(userId);
+    if (boards.isEmpty()) {
+      throw new BoardNotFoundException("No boards found for this user.");
+    }
+
+    logger.info("Successfully retrieved all boards for user with id: {}", userId);
+    return new ResponseEntity<>(boards, HttpStatus.OK);
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<?> getBoardById(@PathVariable Long id) {
-    try {
-      if (id <= 0) {
-        return new ResponseEntity<>("Invalid board ID.", HttpStatus.BAD_REQUEST);
-      }
-
-      Board board = boardService.getBoardById(id);
-      if (board == null) {
-        return new ResponseEntity<>("Board not found.", HttpStatus.NOT_FOUND);
-      }
-
-      return new ResponseEntity<>(board, HttpStatus.OK);
-    } catch (Exception e) {
-      logger.error("Board not found: {}", e.getMessage());
-      return new ResponseEntity<>("An internal error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+    if (id <= 0) {
+      throw new BoardNotFoundException("Invalid board ID.");
     }
+
+    Board board = boardService.getBoardById(id);
+    if (board == null) {
+      throw new BoardNotFoundException("Board not found.");
+    }
+
+    logger.info("Successfully retrieved the board with id: {}", board.getId());
+    return new ResponseEntity<>(board, HttpStatus.OK);
   }
 
   @PostMapping
   public ResponseEntity<?> createBoard(@Valid @RequestBody Board board) {
-    try {
-      // Validate with @Valid
-      Board newBoard = boardService.createBoard(board);
+    Board newBoard = boardService.createBoard(board);
 
-      return new ResponseEntity<>(newBoard, HttpStatus.CREATED);
-    } catch (Exception e) {
-      logger.error("An error occurred: {}", e.getMessage());
-      return new ResponseEntity<>("An internal error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    logger.info("Successfully created a new board with id: {}", newBoard.getId());
+    return new ResponseEntity<>(newBoard, HttpStatus.CREATED);
   }
 
   @PutMapping("/{id}")
   public ResponseEntity<?> updateBoard(@Valid @RequestBody Board board, @PathVariable Long id) {
-    try {
-      // Validate with @Valid
-      Board updatedBoard = boardService.updateBoard(id, board);
+    Board updatedBoard = boardService.updateBoard(id, board);
 
-      return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
-
-    } catch (Exception e) {
-      logger.error("An error occurred: {}", e.getMessage());
-      return new ResponseEntity<>("An internal error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    logger.info("Successfully updated the board with id: {}", updatedBoard.getId());
+    return new ResponseEntity<>(updatedBoard, HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<?> deleteBoard(@PathVariable Long id) {
-    try {
-      if (id < 0) {
-        return new ResponseEntity<>("Invalid board ID.", HttpStatus.BAD_REQUEST);
-      }
-
-      boardService.deleteBoard(id);
-
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    } catch (Exception e) {
-      logger.error("Board not found: {}", e.getMessage());
-      return new ResponseEntity<>("An internal error occurred.", HttpStatus.INTERNAL_SERVER_ERROR);
+    if (id < 0) {
+      throw new BoardNotFoundException("Invalid board ID.");
     }
+    boardService.deleteBoard(id);
+
+    logger.info("Successfully deleted the board with id: {}", id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 }
