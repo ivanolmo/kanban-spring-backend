cc8c40a011f6fc78d3709acd03788d216784a3a9
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-11 00:00:06 -0500

    Refactor Board-related layers to use DTOs
    Board controller, service, and repository now use the DTOs to pass data between components.

--- src/main/java/com/ivanolmo/kanbantaskmanager/repository/BoardRepository.java
+++ src/main/java/com/ivanolmo/kanbantaskmanager/repository/BoardRepository.java
@@ -2,12 +2,15 @@ package com.ivanolmo.kanbantaskmanager.repository;
 
 import com.ivanolmo.kanbantaskmanager.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
   Optional<List<Board>> findByUserId(Long userId);
-  Board findBoardByBoardNameAndUserId(String boardName, Long userId);
+  @Query("SELECT b FROM Board b WHERE LOWER(b.boardName) = LOWER(:boardName) AND b.user.id = :userId")
+  Optional<Board> findBoardByBoardNameAndUserId(@Param("boardName") String boardName, @Param("userId") Long userId);
   Boolean existsByBoardNameAndUserIdAndIdNot(String boardName, Long userId, Long id);
 }
