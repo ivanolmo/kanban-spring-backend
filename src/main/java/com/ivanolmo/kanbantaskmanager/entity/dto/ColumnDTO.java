42fe3189a43847b8b1b3b33718abeeda6f6120f0
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-12 11:43:31 -0500

    Add dto class for board columns

---
+++ src/main/java/com/ivanolmo/kanbantaskmanager/entity/dto/ColumnDTO.java
@@ -0,0 +1,12 @@
+package com.ivanolmo.kanbantaskmanager.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ColumnDTO {
+  private String columnName;
+}
