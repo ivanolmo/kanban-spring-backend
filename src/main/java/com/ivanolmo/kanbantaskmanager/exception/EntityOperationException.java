9b76716de18119b868903158a2acb79b0d21aa6e
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-17 01:24:28 -0500

    Add general entity-related exception handler
    This project had way too many custom exceptions which were all nearly identical to each other. This general handler will replace them all and functionality should remain unchanged.

---
+++ src/main/java/com/ivanolmo/kanbantaskmanager/exception/EntityOperationException.java
@@ -0,0 +1,18 @@
+package com.ivanolmo.kanbantaskmanager.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class EntityOperationException extends BaseException {
+  public EntityOperationException(String entity,
+                                  String operation,
+                                  HttpStatus status) {
+    super(entity + " " + operation + " operation failed", status);
+  }
+
+  public EntityOperationException(String entity,
+                                  String operation,
+                                  Throwable cause,
+                                  HttpStatus status) {
+    super(entity + " " + operation + " operation failed", cause, status);
+  }
+}
