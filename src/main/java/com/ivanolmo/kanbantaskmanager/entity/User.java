1260483ae17b0499b7325ed2ab97188519ee40f3
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-18 21:29:12 -0500

    Refactor User entity for Spring Security

--- src/main/java/com/ivanolmo/kanbantaskmanager/entity/User.java
+++ src/main/java/com/ivanolmo/kanbantaskmanager/entity/User.java
@@ -1,33 +1,38 @@
 package com.ivanolmo.kanbantaskmanager.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class User implements UserDetails {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String name;
-
-  @jakarta.persistence.Column(unique = true)
+  @jakarta.persistence.Column(unique = true, nullable = false)
   private String email;
 
-  @jakarta.persistence.Column(name = "email_verified")
-  private LocalDateTime emailVerified;
-
-  private String image;
+  @jakarta.persistence.Column(nullable = false)
+  private String password;
 
   @CreatedDate
   @jakarta.persistence.Column(name = "created_at", nullable = false, updatable = false)
@@ -40,11 +45,38 @@ public class User {
   @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<Board> boards = new ArrayList<>();
 
-  // spring security
-  private String password;
-
   @Override
   public String toString() {
-    return "User [id=" + id + ", name=" + name + "]";
+    return "User [id=" + id + "]";
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 }
