f9e3f61d203c2f91144985429abd38ab455f6a37
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-22 14:38:51 -0500

    Refactored Board controller to use standard ApiResponse

--- src/main/java/com/ivanolmo/kanbantaskmanager/controller/BoardController.java
+++ src/main/java/com/ivanolmo/kanbantaskmanager/controller/BoardController.java
@@ -4,12 +4,10 @@ import com.ivanolmo.kanbantaskmanager.dto.BoardCreationRequestDTO;
 import com.ivanolmo.kanbantaskmanager.dto.BoardDTO;
 import com.ivanolmo.kanbantaskmanager.dto.ColumnDTO;
 import com.ivanolmo.kanbantaskmanager.service.BoardService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,57 +23,56 @@ public class BoardController {
   }
 
   @GetMapping
-  public ResponseEntity<List<BoardDTO>> getAllUserBoards(@RequestParam String userId,
-                                                         HttpServletRequest request) {
-    CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-    if (csrfToken != null) {
-      System.out.println("CSRF token from request attribute: " + csrfToken.getToken());
-    }
-
+  public ResponseEntity<ApiResponse<List<BoardDTO>>> getAllUserBoards(@RequestParam String userId) {
     List<BoardDTO> boards = boardService.getAllUserBoards(userId);
 
     log.info("Successfully retrieved all boards for user with id: {}", userId);
-    return new ResponseEntity<>(boards, HttpStatus.OK);
+    return ApiResponseUtil.buildSuccessResponse(
+        boards, "Successfully retrieved all boards for the user", HttpStatus.OK);
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<BoardDTO> getBoardById(@PathVariable String id) {
+  public ResponseEntity<ApiResponse<BoardDTO>> getBoardById(@PathVariable String id) {
     BoardDTO board = boardService.getBoardById(id);
 
     log.info("Successfully retrieved the board with id: {}", id);
-    return new ResponseEntity<>(board, HttpStatus.OK);
+    return ApiResponseUtil.buildSuccessResponse(
+        board, "Successfully retrieved the board by id", HttpStatus.OK);
   }
 
   @GetMapping("/{id}/columns")
-  public ResponseEntity<List<ColumnDTO>> getAllColumnsForBoard(@PathVariable String id) {
+  public ResponseEntity<ApiResponse<List<ColumnDTO>>> getAllColumnsForBoard(@PathVariable String id) {
     List<ColumnDTO> columns = boardService.getAllColumnsForBoard(id);
 
     log.info("Successfully retrieved all columns for board with id: {}", id);
-    return new ResponseEntity<>(columns, HttpStatus.OK);
+    return ApiResponseUtil.buildSuccessResponse(
+        columns, "Successfully retrieved all columns for the board", HttpStatus.OK);
   }
 
   @PostMapping
-  public ResponseEntity<BoardDTO> addBoardToUser(@Valid @RequestBody BoardCreationRequestDTO request) {
+  public ResponseEntity<ApiResponse<BoardDTO>> addBoardToUser(@Valid @RequestBody BoardCreationRequestDTO request) {
     BoardDTO newBoardDTO = boardService.addBoardToUser(request.getUserId(), request.getBoard());
 
     log.info("Successfully created a new board with id: {}", newBoardDTO.getId());
-    return new ResponseEntity<>(newBoardDTO, HttpStatus.CREATED);
+    return ApiResponseUtil.buildSuccessResponse(
+        newBoardDTO, "Successfully created the board", HttpStatus.CREATED);
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<BoardDTO> updateBoardName(@Valid @RequestBody BoardDTO boardDTO,
-                                                  @PathVariable String id) {
+  public ResponseEntity<ApiResponse<BoardDTO>> updateBoardName(@Valid @RequestBody BoardDTO boardDTO,
+                                                               @PathVariable String id) {
     BoardDTO updatedBoardDTO = boardService.updateBoardName(id, boardDTO);
 
     log.info("Successfully updated the board with id: {}", id);
-    return new ResponseEntity<>(updatedBoardDTO, HttpStatus.OK);
+    return ApiResponseUtil.buildSuccessResponse(
+        updatedBoardDTO, "Successfully updated the board", HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<Void> deleteBoard(@PathVariable String id) {
+  public ResponseEntity<ApiResponse<Void>> deleteBoard(@PathVariable String id) {
     boardService.deleteBoard(id);
 
     log.info("Successfully deleted the board with id: {}", id);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    return ApiResponseUtil.buildSuccessResponse(null, null, HttpStatus.NO_CONTENT);
   }
 }
