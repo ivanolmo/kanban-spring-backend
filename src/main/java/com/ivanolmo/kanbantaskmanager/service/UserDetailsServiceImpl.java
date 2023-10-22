a4d2e91e7347e3fd71a7de302a0a35c8bfd3775c
Ivan Olmo <ivan.olmo@gmail.com>
2023-10-22 17:43:18 -0500

    Created new implementations of UserDetails and UserDetailsService
    UserDetails was previously implemented directly on the User entity. Now it is implemented in its own class.
    
    UserDetailsService had no implementation either and was being injected from the ApplicationConfig. Now it is implemented in its own class.

---
+++ src/main/java/com/ivanolmo/kanbantaskmanager/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.ivanolmo.kanbantaskmanager.service;
+
+import com.ivanolmo.kanbantaskmanager.entity.User;
+import com.ivanolmo.kanbantaskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+    return new UserDetailsImpl(user);
+  }
+}
