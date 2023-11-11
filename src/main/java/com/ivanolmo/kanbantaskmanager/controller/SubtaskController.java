9b20796a7b470870c2c7d0e570cec0edd27a0aa8
Ivan Olmo <ivan.olmo@gmail.com>
2023-11-11 02:01:12 -0600

    Refactor to remove unused controller methods
    Remaining method is used to toggle a subtask's completion status.

--- src/main/java/com/ivanolmo/kanbantaskmanager/controller/SubtaskController.java
+++ src/main/java/com/ivanolmo/kanbantaskmanager/controller/SubtaskController.java
@@ -1,47 +1,29 @@
 package com.ivanolmo.kanbantaskmanager.controller;
 
-import com.ivanolmo.kanbantaskmanager.dto.SubtaskCreationRequestDTO;
 import com.ivanolmo.kanbantaskmanager.dto.SubtaskDTO;
 import com.ivanolmo.kanbantaskmanager.service.SubtaskService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/subtasks")
+@RequestMapping("/subtasks")
 @RequiredArgsConstructor
 @Slf4j
 public class SubtaskController {
   private final SubtaskService subtaskService;
 
-  @PostMapping
-  public ResponseEntity<ApiResponse<SubtaskDTO>> addSubtaskToTask(@Valid @RequestBody SubtaskCreationRequestDTO request) {
-    SubtaskDTO newSubtaskDTO = subtaskService.addSubtaskToTask(request.getTaskId(),
-        request.getSubtask());
-
-    log.info("Successfully added a new subtask to task with id: {}", request.getTaskId());
-    return ApiResponseUtil.buildSuccessResponse(
-        newSubtaskDTO, "Successfully created the subtask", HttpStatus.CREATED);
-  }
-
   @PutMapping("/{id}")
-  public ResponseEntity<ApiResponse<SubtaskDTO>> updateSubtask(@Valid @RequestBody SubtaskDTO subtaskDTO,
-                                                  @PathVariable String id) {
-    SubtaskDTO updatedSubtaskDTO = subtaskService.updateSubtask(id, subtaskDTO);
+  public ResponseEntity<ApiResponse<SubtaskDTO>> toggleSubtaskCompletion(@PathVariable String id) {
+    SubtaskDTO toggledSubtaskDTO = subtaskService.toggleSubtaskCompletion(id);
 
-    log.info("Successfully updated the subtask with id: {}", id);
+    log.info("Successfully toggled the subtask with id: {}", id);
     return ApiResponseUtil.buildSuccessResponse(
-        updatedSubtaskDTO, "Successfully updated the subtask", HttpStatus.OK);
-  }
-
-  @DeleteMapping("/{id}")
-  public ResponseEntity<ApiResponse<Void>> deleteSubtask(@PathVariable String id) {
-    subtaskService.deleteSubtask(id);
-
-    log.info("Successfully deleted the subtask with id: {}", id);
-    return ApiResponseUtil.buildSuccessResponse(null, null, HttpStatus.NO_CONTENT);
+        toggledSubtaskDTO, "Successfully toggled the subtask completion", HttpStatus.OK);
   }
 }
